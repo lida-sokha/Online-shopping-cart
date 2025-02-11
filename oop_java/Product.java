dcd8dd93b3ff67aebcab7c691d5dd23006f9663e
Sry <soursour3001@gmail.com>
2025-02-11 13:43:57 +0700

    product

--- oop_java/Product.java
+++ oop_java/Product.java
@@ -15,16 +15,5 @@ public class Product {
         this.category = category;
         this.description = description;
     }
-    public String toString() {
-        return "Product ID: " + productId + "\n" +
-               "Name: " + name + "\n" +
-               "Price: " + price + "\n" +
-               "Quantity: " + quantity + "\n" +
-               "Category: " + category + "\n" +
-               "Description: " + description;
-    }  
-    public static void main(String[] args) {
-        Product p1= new Product("11", "milk", 5, 1, "milk", "h");
-        System.out.println(p1);
-    }
+ 
 }
