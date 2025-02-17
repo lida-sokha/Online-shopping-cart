b0a7f315ebcd99e9be8714adf359b3b58d418606
lida-sokha <sokhalida1234@gmail.com>
2025-02-17 13:52:57 +0700

    delete

--- oop_java/Main.java
+++
@@ -1,8 +0,0 @@
-    public static void main(String[] args) {
-        User newUser = new User("sokha", "sok@gmail.com", "pp", "123", "888","staff");
-        if (newUser.signUp()) {  // Check the return value!
-            System.out.println("Signup successful!");
-        } else {
-            System.out.println("Signup failed. Please check your details or try again.");
-        }
-    }
